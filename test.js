13a44fe220361adcce68ea906cdf43f01c518585
gabriel-severo <gabriel.ivi@hotmail.com>
2020-06-12 10:05:04 -0400

    Bot send status server

--- test.js
+++ test.js
@@ -3,25 +3,44 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const ping = require('minecraft-server-util');
 
-async function teste() {
+async function pingServer() {
     return await ping('codehaunted.aternos.me', 25565);
 }
 
-let canal
+let id, canal, status
+
+async function setChannel(id){
+    return await client.channels.fetch(id).then(channel => {
+        return channel
+    })
+}
+
+async function getStatus(canal){
+    const response = await pingServer()
+    try{
+        if(response.descriptionText.includes('offline') && status !== "offline"){
+            status = "offline"
+            canal.send("O servidor está offline")
+        }else if(Number(response.maxPlayers)>0 && status !== "iniciado"){
+            status = "iniciado"
+            canal.send("Servidor iniciado")
+        }
+    }catch(e){
+        if(status !== "iniciando"){
+            canal.send("O servidor está iniciando")
+        }
+        status = "iniciando"
+    }
+    getStatus(canal)
+}
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    setInterval(async() => {
-        if(canal){
-            const response = await teste()
-            if(response.descriptionText.includes('queue')){
-                const time = response.descriptionText.split('§aca. ')[1].split(' minutes§8.')[0]
-                canal.send(`O servidor está em fila e iniciará em ${time} minutos`)
-            }else if(response.descriptionText.includes('offline')){
-                canal.send("O servidor está offline")
-            }else{
-                canal.send("Servidor iniciado")
-            }
+    const setchannel = setInterval(async () => {
+        if(id){
+            canal = await setChannel(id)
+            clearInterval(setchannel)
+            getStatus(canal)
         }
     }, 1000)
 });
@@ -32,21 +51,24 @@ client.on('message', async msg => {
     const args = msg.content.slice(process.env.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     if (command === 'online') {
-        const response = await teste()
-        if(response.descriptionText.includes('queue')){
-            const time = response.descriptionText.split('§aca. ')[1].split(' minutes§8.')[0]
-            msg.channel.send(`O servidor está em fila e iniciará em ${time} minutos`)
-        }else if(response.descriptionText.includes('offline')){
-            msg.channel.send("O servidor está offline")
-        }else{
-            msg.channel.send("Servidor iniciado")
+        try{
+            const response = await pingServer()
+            if(response.descriptionText.includes('queue')){
+                const time = response.descriptionText.split('§aca. ')[1].split(' minutes§8.')[0]
+                msg.channel.send(`O servidor está em fila e iniciará em ${time} minutos`)
+            }else if(response.descriptionText.includes('offline')){
+                msg.channel.send("O servidor está offline")
+            }else if(Number(response.maxPlayers)>0){
+                msg.channel.send("Servidor iniciado")
+            }else{
+                msg.channel.send("Estado inválido")
+                console.log(response)
+            }
+        }catch(e){
+            msg.channel.send("Servidor iniciando")
         }
-        console.log(response)
     }else if(command === 'setchannel'){
         id = args[0].replace('<#', '').replace('>', '')
-        await client.channels.fetch(id).then(channel => {
-            canal = channel
-        })
     }
 });
 
