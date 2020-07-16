023320201e7f472cc845ff9d4cd942cc80a5677f
Gabriel Rodrigues <gabriel.ivi@hotmail.com>
2020-07-16 19:19:47 -0400

    Now play song

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord.js-commando')
+const ytdl = require('ytdl-core')
 
 module.exports = module.exports = class PlayCommand extends Command {
 	constructor(client) {
@@ -7,13 +8,26 @@ module.exports = module.exports = class PlayCommand extends Command {
 			group: 'music',
 			memberName: 'play',
             description: 'Play a song',
+            args: [
+                {
+                    key: 'query',
+                    prompt: 'What song or playlist would you like to listen to?',
+                    type: 'string',
+                    validate: function(query){
+                        return query.length > 0 && query.length < 200
+                    }
+                }
+            ]
 		});
 	}
 
-	run(message) {
+	run(message, {query}) {
         const voiceChannel = message.member.voice.channel;
         if(!voiceChannel){
             return message.say("Você precisa estar em canal de voz")
         }
+        voiceChannel.join().then((connection) => {
+            connection.play(ytdl(query, {quality: 'highestaudio'}))
+        })
 	}
 };
